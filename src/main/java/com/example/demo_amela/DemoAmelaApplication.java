24e72e2f144329831e2f78fe13b7a0fcd6ab2f0d
Lehung59 <nguyenlehungsc@gmail.com>
2024-04-05 16:43:46 +0700

    buoi1

---
+++ src/main/java/com/example/demo_amela/DemoAmelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_amela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAmelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAmelaApplication.class, args);
+	}
+
+}
